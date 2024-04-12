7f9e67daa11bce22fcb1c4ffa3a12aaa9d38a79c
22JunHee <lees8485@gmail.com>
2024-04-12 17:17:04 +0900

    데이터베이스 mysql로 수정

--- database.py
+++ database.py
@@ -3,9 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "sqlite:///./db.sqlite"
+username = 'dbmasteruser'
+password = 'poiqwq12'
+host = 'ls-0f44ac8781fc8ebd52164e957a25ffc2511f62bc.c10g2og22png.ap-northeast-2.rds.amazonaws.com'
+port = '3306'  # MySQL 기본 포트
+database_name = 'dbmaster'
 
-engine = create_engine(DB_URL, connect_args = { "check_same_thread": False })
+DB_URL = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database_name}"
+
+engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
